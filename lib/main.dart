5fed5c3597329d75097cf711f4fa714ea50791f0
shivamratan <shivamratan61@gmail.com>
2026-08-31 17:16:48 +0530

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/router/app_router.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
+
+void fetchFirebaseConfig() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Fetch the FCM registration token
+  String? token = await FirebaseMessaging.instance.getToken();
+  print("Firebase FCM Token: $token");
+}
+
 void main() {
-  runApp(const MyApp());
+  // Required if you add async initialization (like Firebase or Drift) later
+  WidgetsFlutterBinding.ensureInitialized();
+  fetchFirebaseConfig();
+
+
+  runApp(const ProviderScope(
+          child: const MyApp()
+        )
+  );
 }
 
 class MyApp extends StatelessWidget {
