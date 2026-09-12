bba17d6c7e7d4ea067fb14f9b8cd1d338f825c3b
shivamratan <shivamratan61@gmail.com>
2026-09-12 14:54:24 +0530

    Home Screen Setup

--- lib/features/home/presentation/screen/home_screen.dart
+++ lib/features/home/presentation/screen/home_screen.dart
@@ -1,21 +1,69 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerStatefulWidget {
 
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
 
+}
+
+class _HomeScreenState extends ConsumerState<ConsumerStatefulWidget> {
+
+  int screenIndex = 0;
+
+  final List<Widget> _screens = [
+    const Center(child: Text('Dashboard'),),
+    const Center(child: Text('Products'),),
+    const Center(child: Text('Cart'),),
+    const Center(child: Text('Profile'),),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
             title: Text('Home Screen')
         ),
-        body: const Center(
-            child: Text('Home Screen')
-        )
+        body: IndexedStack(
+          index: screenIndex,
+          children: _screens,
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: screenIndex,
+        onTap: (index) {
+          setState(() {
+            screenIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard_outlined),
+            activeIcon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            activeIcon: Icon(Icons.shopping_bag),
+            label: 'Product',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            activeIcon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 
